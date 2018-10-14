80565838270522acb7603e7bfe8f6e38cbd3d860
ura <androidura@gmail.com>
2018-10-14 15:16:32 +0300

    fix bug with EnumConverter

--- advancedpreference/src/main/java/com/fastsoft/advancedpreference/converters/EnumConverter.java
+++ advancedpreference/src/main/java/com/fastsoft/advancedpreference/converters/EnumConverter.java
@@ -5,14 +5,21 @@ import android.support.annotation.NonNull;
 public class EnumConverter extends PreferenceConverter<Enum,String>{
     @Override
     protected String convertFromFirstToClass(@NonNull Enum from, @NonNull Class<? extends String> classToConvert) {
-        return from.name();
+        return (String) convert(from,classToConvert);
     }
 
     @Override
     protected Enum convertFromSecondToClass(@NonNull String from, @NonNull Class<? extends Enum> classToConvert) {
-        for(Enum possible:classToConvert.getEnumConstants())
-            if(possible.name().equals(from))
-                return possible;
+        return (Enum) convert(from,classToConvert);
+    }
+    public Object convert(@NonNull Object from,@NonNull Class classToConvert){
+        if(from.getClass().isEnum()){
+            return ((Enum)from).name();
+        }else{
+            for(Enum possible:((Class<Enum>)classToConvert).getEnumConstants())
+                if(possible.name().equals(from))
+                    return possible;
+        }
         return null;
     }
 
